d26bd440c5fef20f4aab5090743ffdd7f2ed2f04
Abraham <abrahamugbeshe@gmail.com>
2022-08-28 21:40:16 +0100

    perf: added links

--- components/socials/Socials.tsx
+++ components/socials/Socials.tsx
@@ -3,20 +3,38 @@ import { FaTwitter, FaGithub, FaInstagram } from "react-icons/fa";
 import { CgMail } from "react-icons/cg";
 
 const Socials = () => {
+  const socials = [
+    {
+      icon: <FaTwitter />,
+      url: "https://twitter.com/klefcodes",
+    },
+    {
+      icon: <FaGithub />,
+      url: "https://github.com/klefcodes",
+    },
+    {
+      icon: <FaInstagram />,
+      url: "https://instagram.com/klefcodes",
+    },
+    {
+      icon: <CgMail />,
+      url: `
+        mailto:me@klefcodes.net?subject=Hello%20Klef%20Codes&body=Hi%20Klef%20Codes,%0D%0A%0D%0A`,
+    },
+  ];
+
   return (
     <>
-      <span className="cursor-pointer" title="Twitter">
-        <FaTwitter />
-      </span>
-      <span className="cursor-pointer" title="Github">
-        <FaGithub />
-      </span>
-      <span className="cursor-pointer" title="Instagram">
-        <FaInstagram />
-      </span>
-      <span className="cursor-pointer" title="Instagram">
-        <CgMail />
-      </span>
+      {socials.map((social) => (
+        <a
+          href={social.url}
+          key={social.url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {social.icon}
+        </a>
+      ))}
     </>
   );
 };
