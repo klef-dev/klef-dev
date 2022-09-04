46e091ec992265da273d6a646fa079fe41dba83c
Usman-Hassan-Gu <usmanhassangu@gmail.com>
2022-09-04 13:24:10 +0100

    feat: added color to icons

--- components/Languages/Languages.tsx
+++ components/Languages/Languages.tsx
@@ -16,19 +16,19 @@ const Languages = () => {
         <div className="flex items-center space-x-5 overflow-x-auto pb-3 language-scrollbar">
           <span
             title="JavaScript"
-            className="text-4xl transform transition duration-500 hover:scale-125 cursor-pointer"
+            className="text-6xl text-yellow-500 transform transition duration-500 hover:scale-125 cursor-pointer"
           >
             <SiTypescript />
           </span>
           <span
             title="Python"
-            className="text-4xl transform transition duration-500 hover:scale-125  cursor-pointer"
+            className="text-6xl text-blue-700 transform transition duration-500 hover:scale-125  cursor-pointer"
           >
             <FaPython />
           </span>
           <span
             title="MySql"
-            className="text-4xl transform transition duration-500 hover:scale-125 cursor-pointer"
+            className="text-8xl text-blue-300 transform transition duration-500 hover:scale-125 cursor-pointer"
           >
             <SiMysql />
           </span>
@@ -43,19 +43,19 @@ const Languages = () => {
         <div className="flex items-center space-x-5 overflow-x-auto pb-3 language-scrollbar">
           <span
             title="Nodejs"
-            className="text-5xl transform transition duration-500 hover:scale-125 cursor-pointer"
+            className="text-8xl text-green-800 transform transition duration-500 hover:scale-125 cursor-pointer"
           >
             <DiNodejs />
           </span>
           <span
             title="Flask"
-            className="text-4xl transform transition duration-500 hover:scale-125  cursor-pointer"
+            className="text-6xl transform transition duration-500 hover:scale-125  cursor-pointer"
           >
             <SiFlask />
           </span>
           <span
             title="React Js"
-            className="text-4xl transform transition duration-500 hover:scale-125 cursor-pointer"
+            className="text-7xl text-blue-900 transform transition duration-500 hover:scale-125 cursor-pointer"
           >
             <FaReact />
           </span>
