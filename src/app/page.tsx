03849741fccef66a2315ce9309760e70e2883dfa
Abraham <abrahamugbeshe@gmail.com>
2023-09-28 18:00:41 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -205,7 +205,7 @@ export default function Home() {
                 </button>
             </div>
             <div className="max-w-2xl px-8 text-sm text-gray-500">
-                Yooo 👋🏼, I&apos;m Abraham.
+                Yooo 👋🏼, I&apos;m Abraham 😜.
                 <br />
                 <br />I build{" "}
                 <Link
